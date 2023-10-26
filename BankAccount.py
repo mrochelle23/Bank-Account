be32efb18332df1cbf47f0374c65f6a867325e2d
mrochelle23 <142530319+mrochelle23@users.noreply.github.com>
2023-10-25 20:29:28 -0400

    Update BankAccount.py

--- BankAccount.py
+++ BankAccount.py
@@ -27,7 +27,7 @@ class BankAccount:
     def print_statment(self):
         print(f"{self.full_name}")
         print(f"Account No.: {self.account_number}")
-        print(f"Balance: {self.balance}")
+        print(f"Balance: {self.balance}.00")
 
 
 mitchell_bank_account = BankAccount("Mitchell", "03141592")
