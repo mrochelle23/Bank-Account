c1e22981e3b249f8b12bf810aca4fc55f423ef93
mrochelle23 <142530319+mrochelle23@users.noreply.github.com>
2023-10-25 20:27:20 -0400

    Update BankAccount3.py

--- BankAccount3.py
+++ BankAccount3.py
@@ -1,47 +1,37 @@
 class BankAccount:
-    def __init__(self, full_name, account_number, balance):
+    def __init__(self, full_name, account_number):
         self.full_name = full_name
         self.account_number = account_number
-        self.balance = balance
+        self.balance = 0
     
     def deposit(self, amount):
-        amount = 4000
-        balance = 300 + 4000
-        print(f'Amount deposited: ${amount}.00 New Balance: ${balance}.00')
+        self.balance += amount
+        print(f'Amount deposited: ${amount}.00 -> New Balance: ${self.balance}.00')
     
     def widthdraw(self, amount):
-        amount = 200
-        balance = 4300
-        balance = 4300 - 200
-        print(f'Amount withdrawn: ${amount}.00 new balance: ${balance}.00')
-        if amount > balance:
-            balance = 4300 - 10
+        if amount > self.balance:
             print("Insufficient funds.")
+            self.balance -= 10
+        else:
+            self.balance -= amount
+            print(f'Amount withdrawn: ${amount}.00 -> New balance: ${self.balance}.00')
 
     def get_balance(self, balance):
-        balance = 4100
-        print(f'Account Balance: ${balance}.00')
-        return f'${balance}.00'
+        print(f'Welcome! Your Account Available Balance is: ${balance}.00')
+        return self.balance
     
     def add_interest(self):
-        balance = 4100
-        interest = 4100 *  0.00083
+        interest = self.balance * 0.00083
+        self.balance += interest
     
     def print_statment(self):
-        print(self.full_name)
-        print(f'Account No.: {self.account_number}')
-        print(f'Balance: {self.balance}')
+        print(f"{self.full_name}")
+        print(f"Account No.: {self.account_number}")
+        print(f"Balance: {self.balance}")
 
 
-john_bank_account = BankAccount("John Smith", "****7845", "$4100.00")
-print(john_bank_account.deposit('amount'))
-print(john_bank_account.widthdraw('amount'))
-print(john_bank_account.get_balance('balance'))
-print(john_bank_account.add_interest())
-print(john_bank_account.print_statment())
-
-
-
-# print(f"Account No.: ****{str(self.account_number)[4:]}")
-
-# in my case, account_number is an int. so I first convert it to a string, and slice the string to only show the last four digits.
+joi_bank_account = BankAccount("John Smith", "****7845")
+joi_bank_account.deposit(4000)
+joi_bank_account.print_statment()
+joi_bank_account.widthdraw(200)
+joi_bank_account.print_statment()
